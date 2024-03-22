106dbe15d2228512c76f0cd4e53739e2e7e91b37
Mikhail <46933536+MesseMMP@users.noreply.github.com>
2024-03-22 21:51:55 +0300

    feat: add highscore saving
    Add method to save and load highscore

--- UnityRunGame 03.24.04/Assets/_Source/Scripts/GameManager.cs
+++ UnityRunGame 03.24.04/Assets/_Source/Scripts/GameManager.cs
@@ -1,23 +1,25 @@
+using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.Serialization;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] public GameObject playerPrefab; // Префаб игрока
     private GameObject _playerInstance; // Экземпляр игрока
     [SerializeField] public GameObject gameOverPanel; // Ссылка на панель проигрыша
-    private ulong _currentScore = 0;
-    [SerializeField] public TextMeshProUGUI score;
-    [SerializeField] public TextMeshProUGUI highScore;
-    [SerializeField] public TextMeshProUGUI endScore;
+    private float _currentScore = 0;
+    private float _highScore = 0;
+    [SerializeField] public TextMeshProUGUI scoreText;
+    [SerializeField] public TextMeshProUGUI highScoreText;
+    [SerializeField] public TextMeshProUGUI endScoreText;
 
     private bool _gameEnded = false; // Флаг, указывающий, завершилась ли игра
 
     void Awake()
     {
         SpawnPlayer();
+        _highScore = PlayerPrefs.GetFloat("SaveScore");
     }
 
     void SpawnPlayer()
@@ -31,7 +33,7 @@ public class GameManager : MonoBehaviour
         if (!_gameEnded)
         {
             _gameEnded = true;
-            endScore.text = "Your Score: " + _currentScore / 1000;
+            endScoreText.text = "Your Score: " + (int)Math.Round(_currentScore);
             // Показываем панель проигрыша
             gameOverPanel.SetActive(true);
             Time.timeScale = 0f; // Остановка времени в игре
@@ -51,7 +53,17 @@ public class GameManager : MonoBehaviour
 
     public void UpdateScore()
     {
-        ++_currentScore;
-        score.text = "Score: " + _currentScore / 1000;
+        _currentScore += 1.0f / 1000.0f; // Увеличиваем счетчик на 1 / 1000 каждый кадр
+        scoreText.text = "Score: " + (int)Math.Round(_currentScore);
+        highScoreText.text = "HighScore: " + (int)Math.Round(_highScore);
+        UpdateHighScore();
+    }
+
+    private void UpdateHighScore()
+    {
+        if ((int)Math.Round(_currentScore) <= (int)Math.Round(_highScore)) return;
+        _highScore = _currentScore;
+
+        PlayerPrefs.SetFloat("SaveScore", _highScore);
     }
 }
